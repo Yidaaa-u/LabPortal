acdf7dc92f724ffd7581693dd511941b05864e4e
yida <miaochen12@outlook.com>
2020-10-09 11:19:24 +0800

    陈淼的第一次提交

--- app/Http/Requests/Admin/CommManage/commDelRequest.php
+++ app/Http/Requests/Admin/CommManage/commDelRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Admin;
+namespace App\Http\Requests\Admin\CommManage;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TestRequest extends FormRequest
+class commDelRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class TestRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class TestRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'comment_id' => 'required|string',
         ];
     }
 }
